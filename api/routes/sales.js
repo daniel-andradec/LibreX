10e98266e4428101430564be324a9e56998a695a
Daniel Andrade <danielandradecarmo@gmail.com>
2024-04-23 13:34:01 -0300

    Find all trasactions de um user implementado

--- api/routes/sales.js
+++ api/routes/sales.js
@@ -14,54 +14,10 @@ router.post('/', async (req, res) => {
 });
 
 // GET /sales - Listar todas as vendas
-router.get('/', async (req, res) => {
-    try {
-        const sales = await Sale.findAll();
-        res.send(sales);
-    } catch (error) {
-        res.status(500).send(error);
-    }
-});
-
-// GET /sales/:id - Obter uma venda específica pelo ID
 router.get('/:id', async (req, res) => {
     try {
-        const sale = await Sale.findByPk(req.params.id);
-        if (sale) {
-            res.send(sale);
-        } else {
-            res.status(404).send({ message: 'Sale not found' });
-        }
-    } catch (error) {
-        res.status(500).send(error);
-    }
-});
-
-// PUT /sales/:id - Atualizar uma venda pelo ID
-router.put('/:id', async (req, res) => {
-    try {
-        const sale = await Sale.findByPk(req.params.id);
-        if (sale) {
-            await sale.update(req.body);
-            res.send(sale);
-        } else {
-            res.status(404).send({ message: 'Sale not found' });
-        }
-    } catch (error) {
-        res.status(400).send(error);
-    }
-});
-
-// DELETE /sales/:id - Deletar uma venda pelo ID
-router.delete('/:id', async (req, res) => {
-    try {
-        const sale = await Sale.findByPk(req.params.id);
-        if (sale) {
-            await sale.destroy();
-            res.send({ message: 'Sale deleted' });
-        } else {
-            res.status(404).send({ message: 'Sale not found' });
-        }
+        const sales = await SaleService.findAll(req.params.id);
+        res.send(sales);
     } catch (error) {
         res.status(500).send(error);
     }
